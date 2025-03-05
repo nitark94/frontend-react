1447a63b5749396e3f3fc75bb509a5ef82cf0f7c
nitark94 <nita.rantaknuuttila@gmail.com>
2025-03-05 13:04:48 +0200

    Lisätty app.jsx importit oikein

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
+// src/App.jsx
 import React from 'react';
+import Header from './components/Header';  // Header-komponentti
+import Course from './components/Course';  // Course-komponentti
 
 const App = () => {
-  console.log('Test log: App is rendering');
   const course = {
-    name: 'React Basics',
+    name: 'Half Stack application development',
+    id: 1,
     parts: [
-      { id: 1, name: 'Fundamentals of React' },
-      { id: 2, name: 'Using Props and State' },
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
     ]
   };
 
   return (
     <div>
-      <h1>{course.name}</h1>
+      <Course course={course} />  {/* Välitetään course-prop Course-komponentille */}
     </div>
   );
-};
+}
 
 export default App;
+
